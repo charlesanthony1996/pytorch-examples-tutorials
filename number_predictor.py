147763a02931ca1d57ead9f35734b5eab256186e
cc211002 <cc211002@fhstp.ac.at>
2023-03-24 12:12:08 +0100

    latest

---
+++ number_predictor.py
@@ -0,0 +1,7 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
