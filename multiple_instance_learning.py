2a6976cb7a9f83657827e22d5cb9a0e0200d52c9
Charles Anthony <anthony@cidanmachinery.com>
2023-08-28 16:59:09 +0200

    minor changes to multiple instance learning classfication

--- multiple_instance_learning.py
+++ multiple_instance_learning.py
@@ -108,10 +108,103 @@ class MILAttentionLayer(layers.Layer):
             trainable=True,
         )
 
-        self.w_weight_params = 
+        self.w_weight_params = self.add_weight(
+            shape=(input_dim, self.weight_params_dim),
+            initializer = self.w_init,
+            name="w",
+            regularizer = self.w_regularizer,
+            trainable= True
+        )
+
+        if self.use_gated:
+            self.u_weight_params = self.add_weight(
+                shape=(input_dim, self.weight_params_dim),
+                intializer = self.u_init,
+                name="u",
+                regularizer=self.u_regularizer,
+                trainable=True
+            )
+
+        else:
+            self.u_weight_params = None
+
+        self.input_built = True
 
 
+    def call(self, inputs):
+
+        # assigning variables from the number of inputs
+        instances = [self.compute_attention_scores(instance) for instance in inputs]
+
+        # apply softmax over instances such that the output summation is equal to 1
+        alpha = tf.math.softmax(instances, axis=0)
+
+        return [alpha[i] for i in range(alpha.shape[0])]
 
 
+    def compute_attention_scores(self, instance):
+
+        # reserve in case "gated" mechanism used
+        original_instance = instance
+
+        # tanh(v * h_k^T)
+        instance = tf.math.tanh(tf.tensordot(instance, self.v_weight_params, axes= 1))
+
+        # for learning non-linear relations efficiently
+        if self.use_gated:
+
+            instance = instance * tf.math.sigmoid(
+                tf.tensordot(original_instance, self.u_weight_params, axes = 1)
+            )
+
+            return tf.tensordot(instance, self.w_weight_params, axes= 1)
+
+
+def plot(data, labels, bag_class, predictions=None, attention_weights=None):
+
+    labels = np.array(labels).reshape(-1)
+
+    if bag_class == "positive":
+        if predictions is not None:
+            labels = np.where(predictions.argmax(1) == 1)[0]
+            bags = np.array(data)[:, labels[0:plot_size]]
+
+
+        else:
+            labels = np.where(labels == 1)[0]
+            bags = np.array(data)[:, labels[0:plot_size]]
+
+    elif bag_class == "negative":
+        if predictions is not None:
+            labels = np.where(predictions.argmax(1) == 0)[0]
+            bags = np.array(data)[:, labels[0:plot_size]]
+
+        else:
+            labels = np.where(labels == 0)[0]
+            bags = np.array(data)[:, labels[0:plot_size]]
+
+
+    else:
+        print(f"There is no class {bag_class}")
+        return
+
+    
+    print(f"The bag class label is {bag_class}")
+    for i in range(plot_size):
+        figure = plt.figure(figsize=(8, 8))
+        print(f"Bag number: {labels[i]}")
+        for j in range(bag_size):
+            image = bags[j][i]
+            figure.add_subplot(1, bag_size, j+ 1)
+            plt.grid(False)
+            if attention_weights is not None:
+                plt.title(np.around(attention_weights[labels[i][j], 2]))
+            plt.imshow(image)
+        plt.show()
+
+
+# plot some validation data bags per class
+# plot(val_data, val_labels, "positive")
+# plot(val_data, val_labels, "negative")
 
 
