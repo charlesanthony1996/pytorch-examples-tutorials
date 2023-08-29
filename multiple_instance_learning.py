29c9434dd48757ce6595dbe77efeb798d0031c8b
Charles Anthony <anthony@cidanmachinery.com>
2023-08-29 17:35:01 +0200

    some problems with alpha[i] and embeddings[i]

--- multiple_instance_learning.py
+++ multiple_instance_learning.py
@@ -84,7 +84,7 @@ class MILAttentionLayer(layers.Layer):
         self.use_gated = use_gated
 
         self.kernel_initializer = keras.initializers.get(kernel_initializer)
-        self.kernel_regularizer = keras.initializers.get(kernel_regularizer)
+        self.kernel_regularizer = keras.regularizers.get(kernel_regularizer)
 
         self.v_init = self.kernel_initializer
         self.w_init = self.kernel_initializer
@@ -119,7 +119,7 @@ class MILAttentionLayer(layers.Layer):
         if self.use_gated:
             self.u_weight_params = self.add_weight(
                 shape=(input_dim, self.weight_params_dim),
-                intializer = self.u_init,
+                initializer = self.u_init,
                 name="u",
                 regularizer=self.u_regularizer,
                 trainable=True
@@ -147,17 +147,28 @@ class MILAttentionLayer(layers.Layer):
         # reserve in case "gated" mechanism used
         original_instance = instance
 
+        # print shapes
+        print("shapes:")
+        print("original instances: ", original_instance.shape)
+        print("v_weight_params: ", self.v_weight_params.shape)
+        if self.use_gated:
+            print("u_weight_params: ", self.u_weight_params.shape)
+
+
         # tanh(v * h_k^T)
         instance = tf.math.tanh(tf.tensordot(instance, self.v_weight_params, axes= 1))
 
         # for learning non-linear relations efficiently
         if self.use_gated:
 
-            instance = instance * tf.math.sigmoid(
+            gate = instance * tf.math.sigmoid(
                 tf.tensordot(original_instance, self.u_weight_params, axes = 1)
             )
-
-            return tf.tensordot(instance, self.w_weight_params, axes= 1)
+            instance = instance * gate
+            attention_scores = tf.tensordot(instance , self.w_weight_params, axes= 1)
+            
+            # return tf.tensordot(instance, self.w_weight_params, axes= 1)
+            return attention_scores
 
 
 def plot(data, labels, bag_class, predictions=None, attention_weights=None):
@@ -206,5 +217,138 @@ def plot(data, labels, bag_class, predictions=None, attention_weights=None):
 # plot some validation data bags per class
 # plot(val_data, val_labels, "positive")
 # plot(val_data, val_labels, "negative")
+
+
+def create_model(instance_shape):
+    # extract features from inputs
+
+    inputs, embeddings = [], []
+    shared_dense_layer_1 = layers.Dense(128, activation="relu")
+    shared_dense_layer_2 = layers.Dense(64, activation="relu")
+    
+    for _ in range(bag_size):
+        inp = layers.Input(instance_shape)
+        flatten = layers.Flatten()(inp)
+        dense_1 = shared_dense_layer_1(flatten)
+        dense_2 = shared_dense_layer_2(dense_1)
+        inputs.append(inp)
+        embeddings.append(dense_2)
+
+
+    # invoke the attention layer
+    alpha = MILAttentionLayer(
+        weight_params_dim = 256,
+        kernel_regularizer= keras.regularizers.l2(0.01),
+        use_gated =True,
+        name="alpha",
+    )(embeddings)
+
+    # reshape alpha to match the shape of embeddings
+    reshaped_alpha = [
+        layers.Reshape(target_shape=(1, bag_size, 1, alpha[i].shape[-1]))(alpha[i][:, None,None, :])
+        for i in range(len(alpha))
+    ]
+
+    concat_alpha = layers.concatenate(reshaped_alpha, axis= 1)
+
+
+
+    # multiply attention weights with the input layers
+    # multiply_layers = [
+    #     layers.multiply([alpha[i], embeddings[i]]) for i in range(len(alpha))
+    # ]
+
+    multiply_layers = [
+        layers.multiply([concat_alpha[:, i], embeddings[i]]) for i in range(len(embeddings))
+    ]
+
+
+
+    # concatenate layers
+    concat = layers.concatenate(multiply_layers, axis = 2)
+
+    # classification output node
+    output = layers.Dense(2, activation="softmax")(concat)
+
+    return keras.Model(inputs, output)
+
+
+
+def compute_class_weights(labels):
+
+    # count number of positive and negative bags
+    negative_count = len(np.where(labels == 0)[0])
+    positive_count = len(np.where(labels == 1)[1])
+    total_count = negative_count + positive_count
+
+    # build class weight dictionary
+    return {
+        0: (1 / negative_count) * (total_count/ 2),
+        1: (1 / positive_count) * (total_count/ 2),
+    }
+
+
+# build and train the model
+def train(train_data, train_labels, val_data, val_labels, model):
+
+    # train model
+    # prepare callbacks
+    # path where to save best weights
+
+    # take the file name from the wrapper
+    file_path = "/tmp/best_model_weights.h5"
+
+    # initialize model checkpoint callback
+    model_checkpoint = keras.callbacks.ModelCheckpoint(
+        file_path,
+        monitor="val_loss",
+        verbose = 0,
+        mode="min",
+        save_best_only=True,
+        save_weights_only=True
+    )
+
+    early_stopping = keras.callbacks.EarlyStopping(
+        monitor="val_loss", patience= 10, mode="min"
+    )
+
+    # compile model
+    model.compile(
+        optimizer="adam",loss="sparse_categorical_crossentropy", metrics=["accuracy"],
+    )
+
+    
+    # fit model
+    model.fit(
+        train_data,
+        train_labels,
+        validation_data=(val_data, val_labels),
+        epochs=20,
+        class_weight=compute_class_weights(train_labels),
+        batch_size = 1,
+        callbacks=[early_stopping, model_checkpoint],
+        verbose=0
+    )
+
+    # load best weights
+    model.load_weights(file_path)
+
+    return model
+
+
+# building models
+instance_shape = train_data[0][0].shape
+models = [create_model(instance_shape) for _ in range(ensemble_avg_count)]
+
+# show single model architecture
+print(models[0].summary())
+
+# training models
+trained_models = [
+    train(train_data, train_labels, val_data , val_labels, model)
+    for model in tqdm(models)
+]
+
+
 
 
