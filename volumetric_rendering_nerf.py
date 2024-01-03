9ac8ac852e2c18a2191a5e8b78fd2fffef10ef80
Charles Anthony <anthony@cidanmachinery.com>
2024-01-03 17:05:42 +0530

    minor changes

--- volumetric_rendering_nerf.py
+++ volumetric_rendering_nerf.py
@@ -203,5 +203,120 @@ def render_rgb_depth(model, rays_flat, t_vals, rand=True, train=True):
 
     return (rgb, depth_map)
 
-    
+# training
+class NeRF(keras.Model):
+    def __init__(self, nerf_model):
+        super().__init__()
+        self.nerf_model = nerf_model
 
+
+    def compile(self, optimizer, loss_fn):
+        super().compile()
+        self.optimizer = optimizer
+        self.loss_fn = loss_fn
+        self.loss_tracker = keras.metrics.Mean(name="loss")
+        self.psnr_metric = keras.metrics.Mean(name="psnr")
+
+    def train_step(self, inputs):
+        # get the images and the rays
+        (images, rays) = inputs
+        (rays_flat, t_vals) = rays
+
+        with tf.GradientTape() as tape:
+            # get the productions from the model
+            rgb, _ = render_rgb_depth(model = self.nerf_model, rays_flat= rays_flat, t_vals = t_vals, rand=True)
+            loss = self.loss_fn(images, pb)
+
+
+        # get the trainable variables
+        trainable_variables = self.nerf_model.trainable_variables
+
+        # get the gradients of the trainable variables with respect to the loss
+        gradients = tape.gradient(loss, trainable_variables)
+
+        # apply the grads and optimize the model
+        self.optimizers.apply_gradients(zip(gradients, trainable_variables))
+
+        # get the psnr of the reconstructed images and the source images
+        psnr = tf.image.psnr(images, rgb, max_val=1.0)
+
+        # comopute our own metrics
+        self.loss_tracker.update_state(loss)
+        self.psnr_metric.udpate_state(psnr)
+
+        return{'loss': self.loss_tracker.result(), "psnr": self.psnr_metric.result() }
+
+    def test_step(self, inputs):
+        # get the images and the rays
+        (images, rays) = inputs
+        (rays_flat, t_vals) = rays
+
+        # get the predictions from the model
+        rgb, _ = render_rgb_depth(model=self.nerf_model , rays_flat = rays_flat, t_vals= t_vals, rand=True)
+        
+        loss = self.loss_fn(images, rgb)
+
+        # get the psnr of the reconstructed images and the source images
+        psnr = tf.image.psnr(images, rgb, max_val=1.0)
+
+        # compute our own metrics
+        self.loss_tracker.update_state(loss)
+        self.psnr_metric.update_state(psnr)
+
+        return {"loss": self.loss_tracker.result(), "psnr": self.psnr_metric.result()}
+
+        @property
+        def metrics(self):
+            return [self.loss_tracker, self.psnr_metric]
+
+test_imgs , test_rays = next(iter(train_ds))
+test_rays_flat, test_t_vals = test_rays
+
+# print(test_imgs)
+
+loss_list = []
+
+class TrainMonitor(keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs=None):
+        loss = logs["loss"]
+        test_recons_images, depth_maps = render_rgb_depth(
+            model=self.model.nerf_model,
+            rays_flat=test_rays_flat,
+            t_vals=test_t_vals,
+            rand=True,
+            train=False
+        )
+
+        # plot the rgb, depth and the loss plot
+        fig, ax = plt.subplots(nrows= 1, ncols=3, figsize=(20, 5))
+        ax[0].imshow(keras.utils.array_to_img(test_recons_images[0]))
+        ax[0].set_title(f"Predicted image: {epoch:03d}")
+
+        ax[1].imshow(keras.utils.array_to_img(depth_maps[0, ... , None]))
+        ax[1].set_title(f"Depth map: {epoch:03d}")
+
+        ax[2].plot(loss_list)
+        ax[2].set_xticks(np.arange(0, epochs + 1, 5.0))
+        ax[2].set_title(f"Loss plot: {epoch:03d}")
+
+        fig.savefig(f"images/{epoch:03d}.png")
+        plt.show()
+        plt.close()
+
+
+num_pos = h * w * num_samples
+nerf_model = get_nerf_model(num_layers=8, num_pos=num_pos)
+
+model = NeRF(nerf_model)
+model.compile(optimizer = keras.optimizers.Adam(), loss_fn=keras.losses.MeanSquaredError())
+
+# create a directory to save the images during training
+if not os.path.exists("images"):
+    os.makedirs("images")
+
+model.fit(train_ds, validation_data=val_ds, batch_size=batch_size, epochs=epochs, callbacks=[TrainMonitor()])
+
+
+
+
+# print(keras.metrics.Mean("string"))
