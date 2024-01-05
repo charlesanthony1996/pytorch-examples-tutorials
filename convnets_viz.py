86fdd2d88e7a00899860db9868adc6ca408e1c31
Charles Anthony <anthony@cidanmachinery.com>
2024-01-05 10:14:51 +0530

    latest changes to the convnets viz model

--- convnets_viz.py
+++ convnets_viz.py
@@ -42,3 +42,49 @@ def gradient_ascent_step(img, filter_index, learning_rate):
     img += learning_rate * grads
     return loss, img
 
+
+# setting up the end to end filter visualization loop
+def initialize_image():
+    img = tf.random.uniform((1, img_width, img_height, 3))
+    return (img - 0.5) * 0.25
+
+
+def visualize_filter(filter_index):
+    iterations = 20
+    learning_rate = 10.0
+    img = initialize_image()
+    for iteration in range(iterations):
+        loss, img = gradient_ascent_step(img, filter_index, learning_rate)
+
+    img = deprocess_image(img[0].numpy())
+    return loss, img
+
+
+def deprocess_image(img):
+    img -= img.mean()
+    img /= img.std() + 1e-5
+    img *= 0.15
+
+    img = img[25:-25, 25:-25, :]
+
+    img += 0.5
+    img = np.clip(img, 0, 1)
+
+    # convert to rgb array
+    img *= 255
+    img = np.clip(img, 0 , 255).astype("uint8")
+    return img
+
+from IPython.display import Image, display
+
+loss, img = visualize_filter(0)
+keras.utils.save_img("0.png", img)
+
+display(Image("0.png"))
+
+
+# visualize the first 64 filters in the target layer
+all_imgs = []
+
+
+
